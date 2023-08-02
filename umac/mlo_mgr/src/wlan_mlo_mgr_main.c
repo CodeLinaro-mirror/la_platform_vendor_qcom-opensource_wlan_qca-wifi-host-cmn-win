251737791ad07791f69d2c608a4c6b3793afb777
Rakesh Boyina <quic_rboyina@quicinc.com>
2023-08-01 17:03:38 -0700

    qcacmn: Add Extended MLD Capabilities And Operations in Basic ML IE
    Add definitions for sub-fields in Extended MLD Capabilities And Operations
    of Multi-Link IE
    
    Initialize AAL params in Extended MLD CAP and OP subfield of Basic ML IE
    
    Change-Id: I84dfd7e4682d1ed040c1158e09888ad193aabca2
    CRs-Fixed: 3590364
    (cherry picked from commit 163480807fee71c7042b75ca9243ab5e28376dec)
    (cherry picked from commit 9f1c80fcc77533359dd462b94cd4e27cc41ce689)

--- umac/mlo_mgr/src/wlan_mlo_mgr_main.c
+++ umac/mlo_mgr/src/wlan_mlo_mgr_main.c
@@ -737,6 +737,9 @@ static QDF_STATUS mlo_dev_ctx_init(struct wlan_objmgr_vdev *vdev)
 		}
 	}
 
+	ml_dev->mlo_max_recom_simult_links =
+		WLAN_UMAC_MLO_RECOM_MAX_SIMULT_LINKS_DEFAULT;
+
 	mlo_dev_mlpeer_list_init(ml_dev);
 
 	ml_link_lock_acquire(g_mlo_ctx);
