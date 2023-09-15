a3885718a1170b40e0edcef052cc87383ef2b652
aloksing <quic_aloksing@quicinc.com>
2023-09-15 18:49:59 +0530

    qcacmn: Fix duplicate mon desc issue
    We are caching status desc till truncated end reason.
    Now if quota gets over before non truncated end reason
    will exit from srng processing.
    And before next napi if wifi down happens then during
    pdev deinit we are not flushing cached status and packet desc.
    which leads to duplicate desc because after
    init we are resetting freelist to start of desc pool and
    we still process previously cached descs.
    
    Flushing all status and packet descs during mon pdev deinit.
    
    Change-Id: Iaea1115f1660d0b35bf284d9b485ac86bfccf5c6
    CRs-Fixed: 3587299

--- dp/inc/cdp_txrx_mon_struct.h
+++ dp/inc/cdp_txrx_mon_struct.h
@@ -447,6 +447,7 @@ enum cdp_mon_phyrx_abort_reason_code {
  * @mpdu_drop_cnt: Total MPDU drop count
  * @end_of_ppdu_drop_cnt: Total end of ppdu drop count
  * @tlv_drop_cnt: TLV drop count
+ * @pending_desc_count: Pending desc_count during pdev deinit
  */
 struct cdp_pdev_mon_stats {
 #ifndef REMOVE_MON_DBG_STATS
@@ -506,6 +507,7 @@ struct cdp_pdev_mon_stats {
 	uint32_t mpdu_drop_cnt;
 	uint32_t end_of_ppdu_drop_cnt;
 	uint32_t tlv_drop_cnt;
+	uint32_t pending_desc_count;
 };
 
 #ifdef QCA_SUPPORT_LITE_MONITOR
