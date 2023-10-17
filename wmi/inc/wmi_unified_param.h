66d65bbfa4364790e454c28a1562ae03d6a3a976
Sri Vidya Gunturi <quic_sgunturi@quicinc.com>
2023-10-17 12:54:30 +0530

    qcacmn: Fix for Double Peer Assoc failure
    Assoc failure in case of MLD mac same as assoc
    link mac address due to 4-way handshake failure.
    In this case, PEER_ASSOC has been received twice on
    STA side and and in 2nd assoc is_new_assoc is not
    set indicating the change in SGI value in the HT CAPs.
    So FW is not sending PEER_MAP_EVENT to host.
    WMI_PEER_EXT_IS_HT_CAPS_CHANGD is set before sending
    peer assoc to indicate the change in the HTCAPs from
    Host Side to FW.
    WAR change
    
    Change-Id: I19f8d96e7531cb227c568710fe36ed61427b1389
    CRs-Fixed: 3641794
    (cherry picked from commit 2902a981f25e61f446b12c8093ea241bb416f3c7)

--- wmi/inc/wmi_unified_param.h
+++ wmi/inc/wmi_unified_param.h
@@ -1426,6 +1426,7 @@ struct peer_assoc_ml_partner_links {
  * @mesh_node_flag: if node is 4 addr node
  * @is_assoc_vdev: true if assoc vdev
  * @peer_dms_capable: is peer DMS capable
+ * @peer_htcap_vhtcap_updated: is peer HT/VHT cap changed/updated
  * @reserved: spare bits
  * @t2lm_params: TID-to-link mapping params
  */
@@ -1516,7 +1517,8 @@ struct peer_assoc_params {
 	bool is_assoc_vdev;
 #endif
 	uint8_t peer_dms_capable:1,
-		reserved:7;
+		peer_htcap_vhtcap_updated:1,
+		reserved:6;
 #ifdef WLAN_FEATURE_11BE
 	struct wmi_host_tid_to_link_map_params t2lm_params;
 #endif
