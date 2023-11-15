dfb3e67d46ecdcbbff5f06ad3e842a684bdefddb
Gaurav Saini <quic_gaursain@quicinc.com>
2023-11-15 17:12:41 +0530

    qcacmn: Fix RCU sall with sanity test case
    With client connect/disconnect test case RCU stall got reported
    due to the the RX buffers being mapped/unmapped at every pipe
    enable/disable.
    
    Fix is move the mapping of RX buffers at IPA init, same as TX buffers.
    
    Change-Id: I6338a4aa9211453707cd6d4dc16a85404eedc495

--- dp/inc/cdp_txrx_ipa.h
+++ dp/inc/cdp_txrx_ipa.h
@@ -770,6 +770,67 @@ cdp_ipa_tx_buf_smmu_unmapping(ol_txrx_soc_handle soc, uint8_t pdev_id,
 	return QDF_STATUS_SUCCESS;
 }
 
+/**
+ * cdp_ipa_rx_buf_smmu_mapping() - Create SMMU mappings for Rx
+ *				   buffers allocated to IPA
+ * @soc: data path soc handle
+ * @pdev_id: device instance id
+ * @line: line number
+ * @func: function name
+ *
+ * Create SMMU mappings for Tx buffers allocated to IPA
+ *
+ * return QDF_STATUS_SUCCESS
+ */
+static inline QDF_STATUS
+cdp_ipa_rx_buf_smmu_mapping(ol_txrx_soc_handle soc, uint8_t pdev_id,
+			    const char *func, uint32_t line)
+{
+	if (!soc || !soc->ops || !soc->ops->ipa_ops) {
+		QDF_TRACE(QDF_MODULE_ID_DP, QDF_TRACE_LEVEL_FATAL,
+			  "%s invalid instance", __func__);
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	if (soc->ops->ipa_ops->ipa_rx_buf_smmu_mapping)
+		return soc->ops->ipa_ops->ipa_rx_buf_smmu_mapping(soc, pdev_id,
+								  func,
+								  line);
+
+	return QDF_STATUS_SUCCESS;
+}
+
+/**
+ * cdp_ipa_rx_buf_smmu_unmapping() - Release SMMU mappings for Rx
+ *				     buffers allocated to IPA
+ * @soc: data path soc handle
+ * @pdev_id: device instance id
+ * @line: line number
+ * @func: function name
+ *
+ * Release SMMU mappings for Tx buffers allocated to IPA
+ *
+ * return QDF_STATUS_SUCCESS
+ */
+static inline QDF_STATUS
+cdp_ipa_rx_buf_smmu_unmapping(ol_txrx_soc_handle soc, uint8_t pdev_id,
+			      const char *func, uint32_t line)
+{
+	if (!soc || !soc->ops || !soc->ops->ipa_ops) {
+		QDF_TRACE(QDF_MODULE_ID_DP, QDF_TRACE_LEVEL_FATAL,
+			  "%s invalid instance", __func__);
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	if (soc->ops->ipa_ops->ipa_rx_buf_smmu_unmapping)
+		return soc->ops->ipa_ops->ipa_rx_buf_smmu_unmapping(soc,
+								    pdev_id,
+								    func,
+								    line);
+
+	return QDF_STATUS_SUCCESS;
+}
+
 #ifdef IPA_WDS_EASYMESH_FEATURE
 /**
  * cdp_ipa_ast_create() - Create/update AST entry in AST table
--- dp/wifi3.0/dp_rx.c
+++ dp/wifi3.0/dp_rx.c
@@ -364,10 +364,12 @@ dp_pdev_nbuf_alloc_and_map_replenish(struct dp_soc *dp_soc,
 
 	nbuf_frag_info_t->paddr =
 		qdf_nbuf_get_frag_paddr((nbuf_frag_info_t->virt_addr).nbuf, 0);
-		dp_ipa_handle_rx_buf_smmu_mapping(dp_soc, (qdf_nbuf_t)(
+		ret = dp_ipa_handle_rx_buf_smmu_mapping(dp_soc, (qdf_nbuf_t)(
 						  (nbuf_frag_info_t->virt_addr).nbuf),
 						  rx_desc_pool->buf_size,
 						  true, __func__, __LINE__);
+		if (ret != QDF_STATUS_SUCCESS)
+			dp_err("Error Failed to map the buffer");
 
 	ret = dp_check_paddr(dp_soc, &((nbuf_frag_info_t->virt_addr).nbuf),
 			     &nbuf_frag_info_t->paddr,
@@ -3044,7 +3046,7 @@ dp_pdev_rx_buffers_attach(struct dp_soc *dp_soc, uint32_t mac_id,
 			hal_rxdma_buff_addr_info_set(dp_soc->hal_soc ,rxdma_ring_entry, paddr,
 						     desc_list->rx_desc.cookie,
 						     rx_desc_pool->owner);
-
+/*
 			dp_ipa_handle_rx_buf_smmu_mapping(
 					dp_soc, nbuf,
 					rx_desc_pool->buf_size, true,
@@ -3055,7 +3057,7 @@ dp_pdev_rx_buffers_attach(struct dp_soc *dp_soc, uint32_t mac_id,
 								     QDF_NBUF_CB_PADDR(nbuf)),
 					  QDF_NBUF_CB_PADDR(nbuf),
 					  rx_desc_pool->buf_size);
-
+*/
 			desc_list = next;
 		}
 
