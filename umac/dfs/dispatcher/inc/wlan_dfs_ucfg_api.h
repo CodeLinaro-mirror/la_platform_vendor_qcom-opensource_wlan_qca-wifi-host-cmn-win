b8d84ce4b327dec13e1577894aea3ee4174b3b9e
Ashishkumar Vijayakumar <quic_ashivija@quicinc.com>
2024-02-09 19:09:29 +0530

    Revert "Revert "Revert "qcacmn: Add support to send dfs nl events"""
    This reverts commit acfb1f82d2f951c9b1e810e63287a681b7f4a874.
    
    Change-Id: Id76ce375d71b1de4c02b968a775c8e019ff46b0b
    Signed-off-by: Ashishkumar Vijayakumar <quic_ashivija@quicinc.com>

--- umac/dfs/dispatcher/inc/wlan_dfs_ucfg_api.h
+++ umac/dfs/dispatcher/inc/wlan_dfs_ucfg_api.h
@@ -30,7 +30,6 @@
 #include <wlan_objmgr_pdev_obj.h>
 #include <wlan_dfs_ioctl.h>
 #include <wlan_dfs_public_struct.h>
-#include <wlan_objmgr_vdev_obj.h>
 
 /**
  * struct dfs_to_mlme - These are MLME function pointer used by DFS component.
@@ -79,7 +78,6 @@
  * @mlme_proc_spoof_success:           Called when FW send spoof success event.
  * @mlme_set_tx_flag:                  Called when Radar is detected to
  *                                     indicate stop data traffic.
- * @mlme_send_dfs_cfg_event:           Send dfs events to user space.
  * @mlme_is_pdev_valid_for_curhwmode:  Validates if the given input pdev is
  *                                     valid for the current HW mode.
  */
@@ -220,9 +218,6 @@ struct dfs_to_mlme {
 	QDF_STATUS (*mlme_set_tx_flag)(struct wlan_objmgr_pdev *pdev,
 				       bool is_tx_allowed);
 #ifndef MOBILE_DFS_SUPPORT
-	void (*mlme_send_dfs_cfg_event)(struct wlan_objmgr_pdev *pdev,
-					struct wlan_channel *wlan_chan,
-					QDF_RADAR_EVENT event);
 	bool (*mlme_is_pdev_valid_for_curhwmode)(struct wlan_objmgr_pdev *pdev);
 #endif
 };
