27aeacdcca4ec35afd62a87fcaf0cef44393ab7d
Thirusenthil Kumaran J <quic_thirusen@quicinc.com>
2024-10-15 18:39:11 +0530

    qcacmn: Add new reg data structures for HW blacklist
    Add the reg data structures for HW blacklist. This is used to store
    the blacklisted channels for each power mode.
    
    Change-Id: If0132a63829f0597ac73d181935c2e17ade5b3e7

--- umac/regulatory/dispatcher/inc/reg_services_public_struct.h
+++ umac/regulatory/dispatcher/inc/reg_services_public_struct.h
@@ -1253,6 +1253,99 @@ struct cur_fcc_rule {
 };
 #endif
 
+#ifndef CONFIG_REG_CLIENT
+enum hw_black_list_status_code {
+	REG_HW_BLACKLIST_CHAN_SP_UPDATE = 0,
+	REG_HW_BLACKLIST_CHAN_VLP_UPDATE,
+	REG_HW_BLACKLIST_CHAN_SP_CLEAR,
+	REG_HW_BLACKLIST_CHAN_VLP_CLEAR,
+	REG_HW_BLACKLIST_CHAN_UPDATE_ALL,
+	REG_HW_BLACKLIST_CHAN_CLEAR_ALL,
+	REG_HW_BLACKLIST_CHAN_INVALID = 255,
+};
+
+enum hw_black_list_chan_data_status_code {
+       REG_HW_BLACKLIST_CHAN_MORE = 0,
+       REG_HW_BLACKLIST_CHAN_DONE,
+       REG_HW_BLACKLIST_CHAN_INVALID_DATA = 3,
+};
+
+/**
+ * struct hw_disallowed_full_bw_chan
+ * @blocked_pri_freq: Blocked primary frequency
+ * @max_blocked_bw: Maximum blocked bandwidth
+ * @blocked_320_center_freq: Blocked 320 center frequency
+ *                           Valid only if one of the two 320 cen freq is
+ *                           blocked.
+ */
+struct hw_disallowed_full_bw_chan {
+	qdf_freq_t blocked_pri_freq;
+	enum phy_ch_width max_blocked_bw;
+	qdf_freq_t blocked_320_center_freq;
+};
+
+/**
+ * struct hw_disallowed_punc_chan
+ * @center_freq: Center frequency
+ * @bw: Bandwidth
+ * @num_blocked_punc_patterns: Number of blocked puncture patterns
+ * @blocked_punc_patterns: List of blocked puncture patterns
+ */
+struct hw_disallowed_punc_chan {
+	qdf_freq_t center_freq;
+	enum phy_ch_width bw;
+	uint8_t num_blocked_punc_patterns;
+	uint16_t *blocked_punc_patterns;
+};
+
+/**
+ * struct hw_blacklisted_channel
+ * @num_hw_blacklisted_full_bw_chans: Number of blocked full BW channels
+ * @num_hw_blacklisted_punc_chans: Number of blocked punctured channels
+ * @full_bw_chan: List of full bandwidth blacklisted channels
+ * @punc_chan: List of punctured blacklisted channels
+ */
+struct hw_blacklisted_channel {
+	uint8_t num_hw_blacklisted_full_bw_chans;
+	uint8_t num_hw_blacklisted_punc_chans;
+	struct hw_disallowed_full_bw_chan *full_bw_chan;
+	struct hw_disallowed_punc_chan *punc_chan;
+};
+
+/**
+ * struct hw_blacklist_chan_info
+ * @is_hw_blacklist_info_valid: Flag to indicate if hw blacklist info is valid
+ * @hw_black_list_status: status of hw blacklist
+ * @num_hw_blacklisted_full_bw_chans: number of blocked full BW channels
+ * @num_hw_blacklisted_punc_chans: number of blocked punctured channels
+ * @hw_full_bw_chans: Array of blocked full BW channels
+ * @hw_punc_chans: Array of blocked punctured channels
+ */
+struct hw_blacklist_chan_info {
+	bool is_hw_blacklist_info_valid;
+	enum hw_black_list_status_code hw_black_list_status;
+	bool is_first;
+	enum hw_black_list_chan_data_status_code hw_black_list_chan_data_status;
+	uint8_t num_hw_blacklisted_full_bw_chans[REG_CURRENT_MAX_AP_TYPE];
+	uint8_t num_hw_blacklisted_punc_chans[REG_CURRENT_MAX_AP_TYPE];
+	struct hw_disallowed_full_bw_chan *hw_full_bw_chans[REG_CURRENT_MAX_AP_TYPE];
+	struct hw_disallowed_punc_chan *hw_punc_chans[REG_CURRENT_MAX_AP_TYPE];
+};
+
+
+/**
+ * struct hw_blacklist_chan_reg_info
+ * @psoc: psoc ptr
+ * @phy_id: phy id
+ * @hw_blacklist_chan_info: hw blacklist channel info
+ */
+struct hw_blacklist_chan_reg_info {
+	struct wlan_objmgr_psoc *psoc;
+	uint8_t phy_id;
+	struct hw_blacklist_chan_info hw_blacklist_chan_info;
+};
+#endif
+
 /**
  * struct cur_regulatory_info
  * @psoc: psoc ptr
@@ -1289,6 +1382,7 @@ struct cur_fcc_rule {
  * @num_6g_reg_rules_client: list of number of 6G reg rules for client
  * @reg_rules_6g_ap_ptr: ptr to 6G AP reg rules
  * @reg_rules_6g_client_ptr: list of ptr to 6G client reg rules
+ * @hw_blacklist_chan_info: list of HW blacklisted channels
  * @fcc_rules_ptr: ptr to fcc rules
  * @num_fcc_rules: Number of fcc rules sent by firmware
  * @is_c2c_supp: Flag to check if c2c is supported
@@ -1329,6 +1423,9 @@ struct cur_regulatory_info {
 	uint32_t num_6g_reg_rules_client[REG_CURRENT_MAX_AP_TYPE][REG_MAX_CLIENT_TYPE];
 	struct cur_reg_rule *reg_rules_6g_ap_ptr[REG_CURRENT_MAX_AP_TYPE];
 	struct cur_reg_rule *reg_rules_6g_client_ptr[REG_CURRENT_MAX_AP_TYPE][REG_MAX_CLIENT_TYPE];
+#ifndef CONFIG_REG_CLIENT
+	struct hw_blacklist_chan_info hw_blacklist_chan_info;
+#endif
 #ifdef CONFIG_REG_CLIENT
 	struct cur_fcc_rule *fcc_rules_ptr;
 	uint32_t num_fcc_rules;
@@ -1506,6 +1603,7 @@ struct reg_fw_afc_power_event {
  * @event_type: AFC event type
  * @expiry_info: pointer to information present in the AFC expiry event
  * @power_info: pointer to information present in the AFC power event
+ * @hw_blacklist_chan_info: List of HW blacklisted channels
  */
 struct afc_regulatory_info {
 	struct wlan_objmgr_psoc *psoc;
@@ -1515,6 +1613,9 @@ struct afc_regulatory_info {
 		struct reg_afc_expiry_event *expiry_info;
 		struct reg_fw_afc_power_event *power_info;
 	};
+#ifndef CONFIG_REG_CLIENT
+	struct hw_blacklist_chan_info hw_blacklist_chan_info;
+#endif
 };
 #endif
 
