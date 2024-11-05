2b2b41727a35e3419dfb06dff4da493bf371b4ba
Edayilliam Jayadev <quic_ejayadev@quicinc.com>
2024-11-05 14:37:51 +0530

    qcacmn: Add MLO reconfiguration QDF module
    Add QDF module for MLO reconfiguration feature.
    
    Change-Id: I27a3981a9b08f33ac617198ac5f902c1e3511ebc
    CRs-Fixed: 3966806

--- qdf/inc/qdf_types.h
+++ qdf/inc/qdf_types.h
@@ -520,6 +520,7 @@ typedef bool (*qdf_irqlocked_func_t)(void *);
  * @QDF_MODULE_ID_TELEMETRY_AGENT: Telemetry Agent Module ID
  * @QDF_MODULE_ID_RF_PATH_SWITCH: RF path switch Module ID
  * @QDF_MODULE_ID_MGMT_RX_SRNG: MGMR RX over SRNG Module ID
+ * @QDF_MODULE_ID_MLRECFG: MLO Reconfiguration
  * @QDF_MODULE_ID_MAX: Max place holder module ID
  *
  * New module ID needs to be added in qdf trace along with this enum.
@@ -692,6 +693,7 @@ typedef enum {
 	QDF_MODULE_ID_TELEMETRY_AGENT,
 	QDF_MODULE_ID_RF_PATH_SWITCH,
 	QDF_MODULE_ID_MGMT_RX_SRNG,
+	QDF_MODULE_ID_MLRECFG,
 	QDF_MODULE_ID_ANY,
 	QDF_MODULE_ID_MAX,
 } QDF_MODULE_ID;
