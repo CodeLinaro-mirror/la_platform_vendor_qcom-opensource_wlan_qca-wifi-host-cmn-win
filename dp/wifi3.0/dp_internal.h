2e93c435d0ef7fd1eb354613b019b66fce23d9b9
Kenvish Butani <quic_kbutani@quicinc.com>
2024-11-19 05:09:29 -0800

    qcacmn: IO Coherency changes in Rx path
    IO Coherency changes in Rx path
    
    Change-Id: Id43355b45378e2f82463451bb68466ffaad60c35
    CRs-Fixed: 3978516

--- dp/wifi3.0/dp_internal.h
+++ dp/wifi3.0/dp_internal.h
@@ -63,6 +63,12 @@
 #endif
 #endif
 
+#ifdef CONFIG_IO_COHERENCY
+#define DP_DSB
+#else
+#define DP_DSB qdf_dsb();
+#endif /* CONFIG_IO_COHERENCY */
+
 #define DP_MAX_VLAN_IDS 4096
 #define DP_VLAN_UNTAGGED 0
 #define DP_VLAN_TAGGED_MULTICAST 1
@@ -4476,6 +4482,14 @@ static inline void *dp_srng_dst_get_next(struct dp_soc *dp_soc,
 	return hal_srng_dst_get_next_cached(hal_soc, hal_ring_hdl);
 }
 
+#ifdef CONFIG_IO_COHERENCY
+static inline void *dp_srng_dst_inv_cached_descs(struct dp_soc *dp_soc,
+						 hal_ring_handle_t hal_ring_hdl,
+						 uint32_t num_entries)
+{
+	return NULL;
+}
+#else
 /**
  * dp_srng_dst_inv_cached_descs() - Wrapper function to invalidate cached
  * descriptors
@@ -4494,6 +4508,7 @@ static inline void *dp_srng_dst_inv_cached_descs(struct dp_soc *dp_soc,
 	return hal_srng_dst_inv_cached_descs(hal_soc, hal_ring_hdl,
 					     num_entries);
 }
+#endif /* CONFIG_IO_COHERENCY */
 #else
 static inline void *dp_srng_dst_get_next(struct dp_soc *dp_soc,
 					 hal_ring_handle_t hal_ring_hdl)
