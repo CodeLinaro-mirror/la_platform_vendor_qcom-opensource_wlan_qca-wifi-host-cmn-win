28f3e7d88bda4ab807d1beee6997a8fd9e3f8982
Devender Kumar <quic_kdevende@quicinc.com>
2025-08-11 10:07:40 +0530

    qcacmn: Optimize RX buffer SMMU mapping towards IPA to avoid RCU stall
    Currently with every first client connection all the RX buffers are
    mapped in IPA domain, as part of WDI pipe enablement, which is done
    by taking nested locks.
    
    With High Frequency of client connect/disconnect test case RCU stalls
    were observed.
    
    Fix is move the mapping of RX buffers at IPA init, same as TX buffers
    
    Change-Id: I056159b43d6298e719a012d46a6a5f206c0be6c5

--- dp/wifi3.0/dp_ipa.c
+++ dp/wifi3.0/dp_ipa.c
@@ -227,8 +227,6 @@ QDF_STATUS dp_ipa_handle_rx_buf_smmu_mapping(struct dp_soc *soc,
 				qdf_atomic_read(&soc->ipa_map_allowed))) {
 		if (!create && qdf_nbuf_is_rx_ipa_smmu_map(nbuf)) {
 			DP_STATS_INC(soc, rx.err.ipa_unmap_no_pipe, 1);
-		} else {
-			return QDF_STATUS_SUCCESS;
 		}
 	}
 
@@ -3740,12 +3738,8 @@ QDF_STATUS dp_ipa_enable_pipes(struct cdp_soc_t *soc_hdl, uint8_t pdev_id,
 	DP_IPA_EP_SET_TX_DB_PA(soc, ipa_res);
 
 	if (!wlan_ipa_config_is_opt_wifi_dp_enabled() &&
-	    !wlan_ipa_is_shared_smmu_enabled()) {
+	    !wlan_ipa_is_shared_smmu_enabled())
 		qdf_atomic_set(&soc->ipa_map_allowed, 1);
-		dp_ipa_handle_rx_buf_pool_smmu_mapping(soc, true, false,
-						       __func__, __LINE__,
-						       DP_RX_IPA_SMMU_POOL_MAP_ENABLE_PIPE);
-	}
 
 	result = qdf_ipa_wdi_enable_pipes(hdl);
 	if (result) {
@@ -3754,12 +3748,8 @@ QDF_STATUS dp_ipa_enable_pipes(struct cdp_soc_t *soc_hdl, uint8_t pdev_id,
 			  __func__, result);
 		qdf_atomic_set(&soc->ipa_pipes_enabled, 0);
 		DP_IPA_RESET_TX_DB_PA(soc, ipa_res);
-		if (qdf_atomic_read(&soc->ipa_map_allowed)) {
+		if (qdf_atomic_read(&soc->ipa_map_allowed))
 			qdf_atomic_set(&soc->ipa_map_allowed, 0);
-			dp_ipa_handle_rx_buf_pool_smmu_mapping(
-					soc, false, false, __func__,
-					__LINE__, 0);
-		}
 		return QDF_STATUS_E_FAILURE;
 	}
 
@@ -3802,11 +3792,8 @@ QDF_STATUS dp_ipa_disable_pipes(struct cdp_soc_t *soc_hdl, uint8_t pdev_id,
 	qdf_atomic_set(&soc->ipa_pipes_enabled, 0);
 
 	if (!wlan_ipa_config_is_opt_wifi_dp_enabled() &&
-	    !wlan_ipa_is_shared_smmu_enabled()) {
+	    !wlan_ipa_is_shared_smmu_enabled())
 		qdf_atomic_set(&soc->ipa_map_allowed, 0);
-		dp_ipa_handle_rx_buf_pool_smmu_mapping(soc, false, false,
-						       __func__, __LINE__, 0);
-	}
 
 	return result ? QDF_STATUS_E_FAILURE : QDF_STATUS_SUCCESS;
 }
@@ -4863,6 +4850,40 @@ QDF_STATUS dp_ipa_rx_buf_pool_smmu_mapping(
 					       DP_RX_IPA_SMMU_POOL_MAP_OPT_DP);
 	return QDF_STATUS_SUCCESS;
 }
+QDF_STATUS dp_ipa_rx_buf_smmu_mapping(
+	struct cdp_soc_t *soc_hdl, uint8_t pdev_id,
+	const char *func, uint32_t line)
+{
+	QDF_STATUS ret;
+
+	struct dp_soc *soc = cdp_soc_t_to_dp_soc(soc_hdl);
+
+	if (!qdf_mem_smmu_s1_enabled(soc->osdev)) {
+		dp_debug("SMMU S1 disabled");
+		return QDF_STATUS_SUCCESS;
+	}
+	ret = dp_ipa_handle_rx_buf_pool_smmu_mapping(soc, true, func, line, 0);
+
+	return ret;
+}
+
+QDF_STATUS dp_ipa_rx_buf_smmu_unmapping(
+	struct cdp_soc_t *soc_hdl, uint8_t pdev_id, const char *func,
+	uint32_t line)
+{
+	struct dp_soc *soc = cdp_soc_t_to_dp_soc(soc_hdl);
+
+	if (!qdf_mem_smmu_s1_enabled(soc->osdev)) {
+		dp_debug("SMMU S1 disabled");
+		return QDF_STATUS_SUCCESS;
+	}
+
+	if (dp_ipa_handle_rx_buf_pool_smmu_mapping(soc, false, func, line, 0))
+		return QDF_STATUS_E_FAILURE;
+
+	return QDF_STATUS_SUCCESS;
+}
+
 #ifdef IPA_WDS_EASYMESH_FEATURE
 QDF_STATUS dp_ipa_ast_create(struct cdp_soc_t *soc_hdl,
 			     qdf_ipa_ast_info_type_t *data)
